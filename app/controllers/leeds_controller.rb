50d7891114134eaa0dd561beb410942fc43f03d3
Hashim Gari <hashmaster3k@gmail.com>
2021-02-16 11:28:48 -0700

    add modal for adding new leed

--- app/controllers/leeds_controller.rb
+++ app/controllers/leeds_controller.rb
@@ -2,4 +2,11 @@ class LeedsController < ApplicationController
   def index
     @leeds = Leed.all
   end
+
+  def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
